5364ce83cb54aa77ee6ec0b0acf8c09763f58824
fbbah61 <fbbah61@gmail.com>
2020-05-05 12:03:14 +0200

    modif index.php ajout_paragraphe

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 <body>
     <div class="container">
        <h1 class="alert alert-secondary text-center mt-3"> Mon Site Github</h1>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quia eius, recusandae illo deserunt deleniti doloribus, necessitatibus, explicabo molestias aspernatur aliquam accusantium quisquam! Culpa, modi voluptate inventore vel dolore laudantium.lorem</p>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit a repellat officia, minima dolor nihil corrupti ducimus libero laborum nisi eius neque consectetur voluptatum consequuntur sequi asperiores omnis in provident? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eius autem tempora a, quasi deleniti nemo accusantium officiis ipsa. Facilis aliquam quisquam ex necessitatibus fuga neque inventore tempora aliquid natus!</p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui necessitatibus perferendis aliquam. Quas quos aliquid reprehenderit laudantium excepturi eum culpa illum dicta nisi! Reprehenderit consequatur aliquam mollitia exercitationem aliquid expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sapiente at laborum molestias fugit ex accusantium iusto unde repellat provident placeat reprehenderit similique dolorum excepturi sequi, corrupti asperiores error? Maxime? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit beatae alias facere est earum laboriosam laudantium maxime? Provident, illum voluptas iusto in eius totam id impedit, commodi, sapiente quod deserunt.</p>
     </div>
     
 </body>
