080291cc531786edaebe86996c99a296d84f8de1
fbbah61 <fbbah61@gmail.com>
2020-05-05 14:25:57 +0200

    modif index.php ajout carroussel

--- index.php
+++ index.php
@@ -12,7 +12,22 @@
        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quia eius, recusandae illo deserunt deleniti doloribus, necessitatibus, explicabo molestias aspernatur aliquam accusantium quisquam! Culpa, modi voluptate inventore vel dolore laudantium.lorem</p>
        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit a repellat officia, minima dolor nihil corrupti ducimus libero laborum nisi eius neque consectetur voluptatum consequuntur sequi asperiores omnis in provident? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eius autem tempora a, quasi deleniti nemo accusantium officiis ipsa. Facilis aliquam quisquam ex necessitatibus fuga neque inventore tempora aliquid natus!</p>
        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui necessitatibus perferendis aliquam. Quas quos aliquid reprehenderit laudantium excepturi eum culpa illum dicta nisi! Reprehenderit consequatur aliquam mollitia exercitationem aliquid expedita! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sapiente at laborum molestias fugit ex accusantium iusto unde repellat provident placeat reprehenderit similique dolorum excepturi sequi, corrupti asperiores error? Maxime? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit beatae alias facere est earum laboriosam laudantium maxime? Provident, illum voluptas iusto in eius totam id impedit, commodi, sapiente quod deserunt.</p>
+       <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100" src="https://picsum.photos/id/237/600/200" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="https://picsum.photos/id/237/600/200" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="https://picsum.photos/id/237/600/200" alt="Third slide">
+            </div>
+        </div>
+        </div>
     </div>
-    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>  
 </body>
 </html>
